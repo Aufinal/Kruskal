e7d75250f681f0a7353872a2165ec4747e51d535
Théo Laurent <theo.laurent@gmx.fr>
2014-12-06 15:37:22 +0100

    Rewrite unionfind.

--- unionfind.h
+++ unionfind.h
@@ -3,17 +3,17 @@
 
 typedef struct s_uf {
   int size ;
-  int* tab ;
-  int* len ;
+  int* father ;
+  int* rank ; /* heuristique pour l'algorithme */
 } uf ;
 
 uf uf_create(int size) ;
 
 uf uf_free(uf u) ;
 
-void uf_union(uf u, int i, int j) ;
+void uf_union(uf u, int x, int y) ;
 
-int uf_find(uf u, int i) ;
+int uf_find(uf u, int x) ;
 
 void uf_reset(uf u) ;
 
